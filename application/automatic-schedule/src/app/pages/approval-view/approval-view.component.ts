94057a87e10164b0d215465dfd23bfdd7b948f9e
haovvv73 <haovvv73@gmail.com>
2024-05-26 22:44:44 +0700

    [ui update] approval-view

---
+++ application/automatic-schedule/src/app/pages/approval-view/approval-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-approval-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './approval-view.component.html',
+  styleUrl: './approval-view.component.css'
+})
+export class ApprovalViewComponent {
+
+}
