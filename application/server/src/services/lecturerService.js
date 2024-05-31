2d6f0c84eeb683180062cf420ca9d58afa4c1622
giahao73 <votrangiahao73@gmail.com>
2024-05-31 14:57:46 +0700

    [dao service] done auth service

--- application/server/src/services/lecturerService.js
+++ application/server/src/services/lecturerService.js
@@ -1,43 +1,126 @@
+import { Lecturer } from "../models/lecturer.js"
 import Dbconnection from "./dbConnection.js"
 
 class LecturerService {
-    constructor(){
+    constructor() {
         this.connection = Dbconnection.getInstance().connection
         this.lecturerTable = 'lecturer'
     }
 
     // GET
-    async getUsers(){
+    async getUsers() {
         const query = 'SELECT * FROM ?'
-        const [row] = await this.connection.execute(query,[this.lecturerTable])
+        const [row] = await this.connection.execute(query, [this.lecturerTable])
         const result = []
 
-        
+        if (row.length > 0) {
+            for (let lecturer of row) {
+                result.push(new Lecturer(
+                    lecturer.lecturerID,
+                    lecturer.email,
+                    lecturer.lecturerName,
+                    lecturer.phone,
+                    lecturer.password,
+                    lecturer.gender,
+                    lecturer.faculty,
+                    lecturer.birthday,
+                    lecturer.address)
+                )
+            }
+        }
 
         return result
     }
 
-    getUserById(){
+    async getUserById(id) {
+        const query = "SELECT * FROM ? WHERE lecturerID = ?"
+        const [row] = await this.connection.execute(query, [this.lecturerTable, id])
+        const result = []
 
+        if (row.length > 0) {
+            const lecturer = row[0]
+            result.push(new Lecturer(
+                lecturer.lecturerID,
+                lecturer.email,
+                lecturer.lecturerName,
+                lecturer.phone,
+                lecturer.password,
+                lecturer.gender,
+                lecturer.faculty,
+                lecturer.birthday,
+                lecturer.address)
+            )
+        }
+
+        return result
     }
 
-    getUserByEmail(){
+    async getUserByEmail(email) {
+        const query = "SELECT * FROM ? WHERE lecturerID = ?"
+        const [row] = await this.connection.execute(query, [this.lecturerTable, email])
+        const result = []
 
+        if (row.length > 0) {
+            const lecturer = row[0]
+            result.push(new Lecturer(
+                lecturer.lecturerID,
+                lecturer.email,
+                lecturer.lecturerName,
+                lecturer.phone,
+                lecturer.password,
+                lecturer.gender,
+                lecturer.faculty,
+                lecturer.birthday,
+                lecturer.address)
+            )
+        }
+
+        return result
     }
 
     // DELETE
-    deleteUserById(){
+    async deleteUserById(id) {
+        const query = 'DELETE FROM ? WHERE lecturerID = ?'
+        const result = await this.connection.execute(query, [this.lecturerTable, id])
 
+        return result[0].affectedRows;
     }
 
     // POST
-    saveUser(){
+    async saveUser(lecturer) {
+        const query = 'INSERT INTO ?(email,lecturerName,phone,password,gender,faculty,birthday,address) VALUES(?,?,?,?,?,?,?,?,?)'
+        const result = await this.connection.execute(query, [
+            lecturer.lecturerID,
+            lecturer.email,
+            lecturer.lecturerName,
+            lecturer.phone,
+            lecturer.password,
+            lecturer.gender,
+            lecturer.faculty,
+            lecturer.birthday,
+            lecturer.address
+        ])
 
+        return result[0].affectedRows
     }
 
     // PUT
-    updateUser(){
-
+    async updateUser(lecturer) {
+        const query = 'UPDATE ? SET email = ?,lecturerName = ?,phone = ?,password = ?,gender = ?,faculty = ?,birthday = ?,address = ? WHERE lecturerID = ?'
+        const result = await this.connection.execute(query, [
+            this.lecturerTable,
+            lecturer.email,
+            lecturer.lecturerName,
+            lecturer.phone,
+            lecturer.password,
+            lecturer.gender,
+            lecturer.faculty,
+            lecturer.birthday,
+            lecturer.address,
+            lecturer.lecturerID,
+        ])
+        
+        return result[0].affectedRows
     }
 
 }
